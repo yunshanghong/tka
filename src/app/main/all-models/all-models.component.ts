cad00ef6043f4f0c50e80b14f4f7c6db312779f5
yunshanghong <yunshanghong@gmail.com>
2020-10-22 09:15:56 +0800

    【修改】all-models 移除isAnimated function

--- src/app/main/all-models/all-models.component.ts
+++ src/app/main/all-models/all-models.component.ts
@@ -8,42 +8,8 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AllModelsComponent implements OnInit {
 
-    @ViewChildren('jsAni') sections: QueryList<ElementRef>;
-
-    constructor(@Inject(DOCUMENT) private document: Document) { }
+    constructor() { }
 
     ngOnInit() { }
 
-    @HostListener('window:load', [])
-    onWindowLoaded() {
-        console.log("load")
-        this.onAddIsAnimated();
-    }
-
-    @HostListener('window:scroll', [])
-    onWindowScroll() {
-        this.onAddIsAnimated();
-    }
-
-    // 滑動到該區增加class "is-animated"
-    onAddIsAnimated(): void {
-        console.log(this.sections);
-        this.sections && this.sections.map((element) => {
-            const htmlEl = element.nativeElement as HTMLElement;
-            const isInView = this.onCheckInViewport(htmlEl);
-            const newElement = element;
-            if (isInView && !htmlEl.classList.contains('is-animated')) {
-                newElement.nativeElement.className += ' is-animated'
-            }
-            return newElement;
-        })
-    }
-
-    // 確認元件是否在視窗內
-    onCheckInViewport(el: HTMLElement): boolean {
-        var rect = el.getBoundingClientRect();
-        var isVisible = el.offsetHeight !== 0;
-        console.log(isVisible);
-        return isVisible && rect.bottom >= 0 && rect.right >= 0 && rect.top <= (window.innerHeight || this.document.documentElement.clientHeight) && rect.left <= (window.innerWidth || document.documentElement.clientWidth);
-    }
 }
