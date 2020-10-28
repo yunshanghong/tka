e723551b15270d2746d071e3c180aff2ff9410db
yunshanghong <yunshanghong@gmail.com>
2020-10-28 17:00:11 +0800

    【修改】about 串接

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { EventEmitterService } from 'src/app/services/eventEmitter.service';
+import { InfoService } from 'src/app/services/info.service';
 
 @Component({
     selector: 'app-about',
@@ -8,11 +9,32 @@ import { EventEmitterService } from 'src/app/services/eventEmitter.service';
 })
 export class AboutComponent implements OnInit, AfterViewInit, OnDestroy {
 
+    // 1. Reasons
+    reasons: Array<Object>;
+
+    // 2. Services
+    services: Array<Object>;
+
     @ViewChild('aboutComponent') aboutComponent: ElementRef;
 
-    constructor(private eventEmitterService: EventEmitterService) { }
+    constructor(private infoService: InfoService, private eventEmitterService: EventEmitterService) { }
 
-    ngOnInit() { console.log("about init") }
+    ngOnInit() {
+        console.log("about init");
+        // 1. Reasons
+        this.infoService.getDynamicContentByType({ Type: "Kinto.Reasons" }).subscribe(
+            (response: any) => this.reasons = response,
+            (error) => { console.error(error) },
+            () => { console.log(this.reasons) }
+        )
+
+        // 2. Services
+        this.infoService.getDynamicContentByType({ Type: "Kinto.Services" }).subscribe(
+            (response: Array<Object>) => this.services = response,
+            (error) => { console.error(error) },
+            () => { console.log(this.services) }
+        )
+    }
 
     ngAfterViewInit() { this.eventEmitterService.onLoadingComplete() }
 
